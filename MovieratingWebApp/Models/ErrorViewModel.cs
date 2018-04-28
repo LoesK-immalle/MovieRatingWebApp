d4ebde36fc8b002206ac4217e6eaedd75ed68599
Loes Kenis <loes.kenis@immalle.eu>
2018-04-28 12:41:02 +0200

    Made MVC project

---
+++ MovieratingWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieratingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
